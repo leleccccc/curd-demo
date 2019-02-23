8b40a62cef488dab9e2eb6221051fc80ae471864
leleccccc <1053591392@qq.com>
2019-02-23 16:36:13 +0800

    删除

--- my-douban-master/src/http/apiParams.js
+++
@@ -1,9 +0,0 @@
-const apiParams = {
-  base_url: '/api',
-  // 电影借口,get请求,count为借口请求的条数
-  hotMovie: '/v2/movie/in_theaters',
-  topMovie: '/v2/movie/top250',
-  newMovie: '/v2/movie/coming_soon'
-}
-
-module.exports = apiParams
